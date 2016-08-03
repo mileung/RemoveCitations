a2887ccb7a51a9ab5fc2b5e78f2ad30ea34c4737
MiLeung <michaeleleung@gmail.com>
2016-08-02 22:50:39 -0700

    remove citations from wikipedia pages

--- popup.js
+++ popup.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('dom content loaded!');
+  var button = document.getElementById('button');
+  var action = document.getElementById('action');
+  // storage.sync.get('Action', function(data) {
+  //   action.innerHTML = data.action;
+  // });
+  //
+  // button.addEventListener('click', function() {
+  //   if (action.innerHTML === 'Disable') {
+  //     storage.sync.set({'Action', 'Disable'});
+  //     action.innerHTML = 'Enable';
+  //   } else {
+  //     storage.sync.set({'Action', 'Enable'});
+  //     action.innerHTML = 'Disable';
+  //   }
+  // });
 });
