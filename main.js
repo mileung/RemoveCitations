8d95910fcd0f4dd2c15dec5e3d5e10ff4ce5a1a6
MiLeung <michaeleleung@gmail.com>
2016-08-03 22:42:54 -0700

    disable and enable works

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.body.innerHTML = document.body.innerHTML.replace(/<sup.*<\/sup>/g, '');
+chrome.storage.sync.get('action', function(data) {
+  if (data.action === 'Disable' || !data.action) {
+    document.body.innerHTML = document.body.innerHTML.replace(/<sup.*<\/sup>/g, '');
+  }
+});
+
+
+// var sups = document.getElementsByTagName('sup');
+// for (var i = 0; i < sups.length; i++) {
+//   sups[i].innerHTML = '';
+// }
--- popup.js
+++ popup.js
@@ -1,17 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var button = document.getElementById('button');
+  // var button = document.getElementById('button');
   var action = document.getElementById('action');
-  // storage.sync.get('Action', function(data) {
-  //   action.innerHTML = data.action;
-  // });
-  //
-  // button.addEventListener('click', function() {
-  //   if (action.innerHTML === 'Disable') {
-  //     storage.sync.set({'Action', 'Disable'});
-  //     action.innerHTML = 'Enable';
-  //   } else {
-  //     storage.sync.set({'Action', 'Enable'});
-  //     action.innerHTML = 'Disable';
-  //   }
-  // });
+  chrome.storage.sync.get('action', function(data) {
+    if (data.action) {
+      action.innerHTML = data.action;
+    }
+  });
+  document.getElementById('button').addEventListener('click', toggleAction);
 });
+
+function toggleAction() {
+  if (action.innerHTML.trim() === 'Disable') {
+    action.innerHTML = 'Enable';
+    chrome.storage.sync.set({'action': 'Enable'});
+  } else {
+    action.innerHTML = 'Disable';
+    document.body.innerHTML = document.body.innerHTML.replace(/<sup.*<\/sup>/g, '');
+    chrome.storage.sync.set({'action': 'Disable'});
+  }
+}
+
+function log(content) {
+  chrome.extension.getBackgroundPage().console.log(content);
+}
