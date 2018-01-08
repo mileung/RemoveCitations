5008ebce440fb573f6d32ff4d940a38f2ecb066e
MiLeung <michaeleleung@gmail.com>
2018-01-08 10:54:53 -0800

    1.2

--- src/content.js
+++ src/content.js
@@ -1,5 +1,5 @@
 chrome.storage.sync.get('action', function(data) {
   if (data.action === 'Disable' || !data.action) {
-    document.body.innerHTML = document.body.innerHTML.replace(/\[\d{1,}\]|\[fn\s\d{1,}\]|<sup.+\[.+citation needed.+\]<\/sup>/g, '');
+    document.body.innerHTML = document.body.innerHTML.replace(/\[\d{1,}]|\[fn\s\d{1,}]|\<sup.+\[.+clarification needed.+\]<\/sup>|\[unreliable source?]|<sup.+\[.+citation needed.+]<\/sup>/g, '');
   }
 });
